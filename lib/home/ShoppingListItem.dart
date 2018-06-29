1355c7588476b056daa7ab2e978f286182f722cc
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-06-29 17:21:16 +0530

    added listItem rating widget

--- lib/home/ShoppingListItem.dart
+++ lib/home/ShoppingListItem.dart
@@ -20,10 +20,55 @@ class ListItemStack extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Stack(
+      fit: StackFit.expand,
       children: <Widget>[
         new ListItemImage(),
+        new ListItemRating(),
         new ListItemName(),
       ],
+    );
+  }
+}
+
+class ListItemRating extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topRight,
+      child: new Container(
+        decoration: new BoxDecoration(
+          color: Colors.black.withOpacity(0.5),
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            bottomLeft: Radius.circular(20.0),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: new Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              new Icon(
+                Icons.star,
+                color: Colors.yellow,
+              ),
+              new SizedBox(
+                width: 5.0,
+              ),
+              new Text(
+                "4.9",
+                maxLines: 1,
+                style: new TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
@@ -33,6 +78,7 @@ class ListItemImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Image.network(
       "https://assets.abfrlcdn.com/img/app/product/1/199775-614183-large.jpg",
+      fit: BoxFit.cover,
     );
   }
 }
@@ -43,10 +89,10 @@ class ListItemName extends StatelessWidget {
     return new Align(
       alignment: Alignment.bottomCenter,
       child: new Container(
+        color: Colors.black38,
         constraints: BoxConstraints.expand(
           height: 80.0,
         ),
-        color: Colors.black38,
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Center(
