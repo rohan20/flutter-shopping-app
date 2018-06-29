8d5bea70aa433d1b03eb3ab97a4d2ad73cb266a1
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-06-29 16:20:57 +0530

    added basic list item

--- lib/home/ShoppingListPage.dart
+++ lib/home/ShoppingListPage.dart
@@ -6,11 +6,33 @@ class ShoppingListPage extends StatelessWidget {
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
       home: new Scaffold(
-        body: new Container(
-          color: Colors.white,
-        ),
+        body: new ShoppingListItem(),
         appBar: new AppBar(
           title: new Text("Shopping App"),
+        ),
+      ),
+    );
+  }
+}
+
+class ShoppingListItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      decoration: new BoxDecoration(
+        border: Border.all(
+          color: Colors.black,
+          width: 1.0,
+        )
+      ),
+      margin: const EdgeInsets.all(20.0),
+      child: Center(
+        child: new Text(
+          "List Item 1",
+          style: TextStyle(
+            fontSize: 40.0,
+            color: Colors.black,
+          ),
         ),
       ),
     );
