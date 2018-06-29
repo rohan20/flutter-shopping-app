3cebf29b964e5ece4c8c99fb8cd94d1da0c760e2
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-06-29 16:04:31 +0530

    add MaterialApp widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(new ShoppingList());
-}
+  }
 
 class ShoppingList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.teal,);
+    return MaterialApp(
+      home: new Scaffold(
+        backgroundColor: Colors.yellowAccent,
+        body: new Container(
+          color: Colors.teal,
+        ),
+        appBar: new AppBar(
+          title: new Text("Hello World"),
+        ),
+      ),
+    );
   }
 }
