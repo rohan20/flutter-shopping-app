9fac2b6f56e3a2dc61ecfd43ee8c2d188b02a6c7
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-06-30 09:38:48 +0530

    moved products list to Constants.dart

--- lib/ui/home/StatefulShoppingListPage.dart
+++ lib/ui/home/StatefulShoppingListPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_e_commerce/model/Product.dart';
 import 'package:flutter_e_commerce/ui/home/ShoppingListItem.dart';
+import 'package:flutter_e_commerce/util/Constants.dart';
 
 class StatefulShoppingListPage extends StatefulWidget {
   @override
@@ -22,7 +23,8 @@ class StatefulShoppingListPageState extends State<StatefulShoppingListPage> {
         accentColor: Colors.pinkAccent,
       );
 
-  List<Product> productsList = _productsList();
+  List<Product> productsList = Constants.productsList();
+  bool isSorted = false;
 
   @override
   Widget build(BuildContext context) {
@@ -40,11 +42,8 @@ class StatefulShoppingListPageState extends State<StatefulShoppingListPage> {
             new IconButton(
               icon: new Icon(Icons.sort),
               onPressed: () {
-                print("Sort icon pressed");
                 setState(() {
-                  productsList.sort(
-                    (Product a, Product b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()),
-                  );
+                  toggleItemsSorting();
                 });
               },
             ),
@@ -69,108 +68,16 @@ class StatefulShoppingListPageState extends State<StatefulShoppingListPage> {
     );
   }
 
-  static List<Product> _productsList() {
-    return [
-      new Product(
-          name: "Blue Shirt",
-          rating: 4.2,
-          imageUrl:
-              "https://assets.abfrlcdn.com/img/app/product/1/199775-614183-large.jpg"),
-      new Product(
-          name: "Red Shirt",
-          rating: 4.8,
-          imageUrl:
-              "https://assets.abfrlcdn.com/img/app/product/8/85181-250173-large.jpg"),
-      new Product(
-          name: "Black Shirt",
-          rating: 3.2,
-          imageUrl:
-              "https://assets.myntassets.com/h_240,q_90,w_180/v1/assets/images/2127876/2017/11/23/11511431472633-Roadster-Men-Black-Regular-Fit-Solid-Casual-Shirt-8801511431472500-1_mini.jpg"),
-      new Product(
-          name: "Purple Shirt",
-          rating: 4.8,
-          imageUrl:
-              "https://5.imimg.com/data5/YD/FK/MY-25516998/mens-formal-shirts-500x500.jpg"),
-      new Product(
-          name: "Green Shirt",
-          rating: 4.1,
-          imageUrl:
-              "https://static1.jassets.com/p/See-Designs-Green-Solid-Casual-Shirt-9267-848157-1-pdp_slider_m.webp"),
-      new Product(
-          name: "Blue Shirt",
-          rating: 4.2,
-          imageUrl:
-              "https://assets.abfrlcdn.com/img/app/product/1/199775-614183-large.jpg"),
-      new Product(
-          name: "Red Shirt",
-          rating: 4.8,
-          imageUrl:
-              "https://assets.abfrlcdn.com/img/app/product/8/85181-250173-large.jpg"),
-      new Product(
-          name: "Black Shirt",
-          rating: 3.2,
-          imageUrl:
-              "https://assets.myntassets.com/h_240,q_90,w_180/v1/assets/images/2127876/2017/11/23/11511431472633-Roadster-Men-Black-Regular-Fit-Solid-Casual-Shirt-8801511431472500-1_mini.jpg"),
-      new Product(
-          name: "Purple Shirt",
-          rating: 4.8,
-          imageUrl:
-              "https://5.imimg.com/data5/YD/FK/MY-25516998/mens-formal-shirts-500x500.jpg"),
-      new Product(
-          name: "Green Shirt",
-          rating: 4.1,
-          imageUrl:
-              "https://static1.jassets.com/p/See-Designs-Green-Solid-Casual-Shirt-9267-848157-1-pdp_slider_m.webp"),
-      new Product(
-          name: "Blue Shirt",
-          rating: 4.2,
-          imageUrl:
-              "https://assets.abfrlcdn.com/img/app/product/1/199775-614183-large.jpg"),
-      new Product(
-          name: "Red Shirt",
-          rating: 4.8,
-          imageUrl:
-              "https://assets.abfrlcdn.com/img/app/product/8/85181-250173-large.jpg"),
-      new Product(
-          name: "Black Shirt",
-          rating: 3.2,
-          imageUrl:
-              "https://assets.myntassets.com/h_240,q_90,w_180/v1/assets/images/2127876/2017/11/23/11511431472633-Roadster-Men-Black-Regular-Fit-Solid-Casual-Shirt-8801511431472500-1_mini.jpg"),
-      new Product(
-          name: "Purple Shirt",
-          rating: 4.8,
-          imageUrl:
-              "https://5.imimg.com/data5/YD/FK/MY-25516998/mens-formal-shirts-500x500.jpg"),
-      new Product(
-          name: "Green Shirt",
-          rating: 4.1,
-          imageUrl:
-              "https://static1.jassets.com/p/See-Designs-Green-Solid-Casual-Shirt-9267-848157-1-pdp_slider_m.webp"),
-      new Product(
-          name: "Blue Shirt",
-          rating: 4.2,
-          imageUrl:
-              "https://assets.abfrlcdn.com/img/app/product/1/199775-614183-large.jpg"),
-      new Product(
-          name: "Red Shirt",
-          rating: 4.8,
-          imageUrl:
-              "https://assets.abfrlcdn.com/img/app/product/8/85181-250173-large.jpg"),
-      new Product(
-          name: "Black Shirt",
-          rating: 3.2,
-          imageUrl:
-              "https://assets.myntassets.com/h_240,q_90,w_180/v1/assets/images/2127876/2017/11/23/11511431472633-Roadster-Men-Black-Regular-Fit-Solid-Casual-Shirt-8801511431472500-1_mini.jpg"),
-      new Product(
-          name: "Purple Shirt",
-          rating: 4.8,
-          imageUrl:
-              "https://5.imimg.com/data5/YD/FK/MY-25516998/mens-formal-shirts-500x500.jpg"),
-      new Product(
-          name: "Green Shirt",
-          rating: 4.1,
-          imageUrl:
-              "https://static1.jassets.com/p/See-Designs-Green-Solid-Casual-Shirt-9267-848157-1-pdp_slider_m.webp"),
-    ];
+
+
+  void toggleItemsSorting() {
+    if (isSorted) {
+      productsList = Constants.productsList();
+    } else {
+      productsList.sort(
+        (a, b) => a.rating.compareTo(b.rating),
+      );
+    }
+    isSorted = !isSorted;
   }
 }
